16ef45b5574e2e82cde6dcada2211b0a1f75e1db
kain-1910 <pvcong1910@gmail.com>
2023-09-13 19:22:44 +0700

    upload lab 1

---
+++ Lab1/Controllers/StudentController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lab1.Models;
+namespace Lab1.Controllers
+{
+    [Route("/Admin/Student")]
+    public class StudentController : Controller
+    {
+        private List<Student> students = new List<Student>();
+        
+        public StudentController()
+        {
+            //Tạo danh sách sinh viên với 4 dữ liệu mẫu
+            students = new List<Student>()
+{
+                new Student() { Id = 101, Name = "Hải Nam", Branch = Branch.IT,
+                Gender = Gender.Male,
+                Address = "A1-2018", Email = "nam@g.com" },
+
+                new Student() { Id = 102, Name = "Minh Tú", Branch = Branch.BE,
+                Gender = Gender.Female,
+                Address = "A1-2019", Email = "tu@g.com" },
+
+                new Student() { Id = 103, Name = "Hoàng Phong", Branch = Branch.CE,
+                Gender = Gender.Male,
+                Address = "A1-2020", Email = "phong@g.com" },
+
+                new Student() { Id = 104, Name = "Xuân Mai", Branch = Branch.EE,
+                Gender = Gender.Female,
+                Address = "A1-2021", Email = "mai@g.com" }
+            };
+        }
+        [HttpGet("List")]
+        public IActionResult Index()
+        {
+            return View(students);
+        }
+        [HttpGet("Add")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost("/Admin/Student/Add")]
+        public IActionResult Create(Student student)
+        {
+            students.Add(student);
+            return View("Index",students);
+        }
+    }
+}
---
+++ Lab1/Models/Student.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+
+namespace Lab1.Models
+{
+    public class Student
+    {
+        public Student() { }
+        public Student(int id, string? name, string? email, string? password, Branch? branch, Gender? gender, string? address, DateTime dateOfBirth)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Password = password;
+            Branch = branch;
+            Gender = gender;
+            Address = address;
+            DateOfBirth = dateOfBirth;
+        }
+
+        public int Id { get; set; }
+        public string? Name { get; set; } 
+        public string? Email { get; set; } 
+        public string? Password { get; set; }
+        public Branch? Branch { get; set; }
+        public Gender? Gender { get; set; }
+        public string? Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
